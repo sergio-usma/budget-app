ac1148f7d2b7d90e7ffc5781945332faf381cae0
Sergio Usma <sergioandresusma@hotmail.com>
2023-11-30 12:13:54 -0500

    Add databse validations according to EDR

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  has_many :categories, foreign_key: :author_id, class_name: 'Category'
-  has_many :movements, foreign_key: :author_id, class_name: 'Movement'
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :categories, foreign_key: :author_id, class_name: 'Category'
+  has_many :movements, foreign_key: :author_id, class_name: 'Movement'
+
+  validates :name, presence: true
 end
