14a67db97eb6726617febf006c0119ff4cc1e435
Sergio Usma <sergioandresusma@hotmail.com>
2023-11-30 12:11:27 -0500

    FIX Model connections according EDR

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :category_movements, foreign_key: :category_id
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,3 +1,5 @@
 class Movement < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :category_movements, foreign_key: :movements_id
+
 end
