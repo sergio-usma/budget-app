236edc274128959cdbf801d406eb392e48439b6d
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-01 08:35:46 -0500

    Add basic logic & views for categories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,27 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.all
+  end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def create; end
+  def edit; end
+
+  def create
+    @category = current_user.categories.build(category_params)
+
+    if @category.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
