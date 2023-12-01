cd57c496598d0881578413953f3fe1426ee92855
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-01 08:35:26 -0500

    FIX connections in Models & create seeds for users

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :category_movements, foreign_key: :category_id
-
-  has_one_attached :icon
+  has_many :category_movements, foreign_key: :category_id, dependent: :destroy
+  has_many :movements, through: :category_movements, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
+  validates :name, length: { maximum: 10 }
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,7 +1,9 @@
 class Movement < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :category_movements, foreign_key: :movements_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id, inverse_of: :movements
+  has_many :category_movements, foreign_key: :movements_id, dependent: :destroy
+  has_many :categories, through: :category_movements
 
   validates :name, presence: true
   validates :amount, numericality: { grater_than: 0 }
+  validates :category_ids, presence: true
 end
